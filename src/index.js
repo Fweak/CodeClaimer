fef7844fa7f23f2ae21f1d8eabe24c67ddffe87b
Fweak <33170633+Fweak@users.noreply.github.com>
2020-12-20 17:07:36 -0800

    Updated this monkey!

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const { readFileSync } = require('fs');
 const { safeLoad } = require('js-yaml');
 const Chalk = require('chalk');
 
-let tokens = readFileSync('./src/tokens.txt', 'utf8').replace(/\r/g, '').split('\n');
-let config = safeLoad(readFileSync('./src/config.yaml'));
+process.title = 'CodeClaimer - fweak';
 
-var count = 0;
-var total = 0;
+const tokens = readFileSync('./src/tokens.txt', 'utf-8').split('\n');
+const config = safeLoad(readFileSync('./src/config.yaml'));
 
-process.title = 'CodeClaimer (fweak)';
-for (let token of tokens) {
-    const client = new Client({ fetchAllMembers: false, messageCacheMaxSize: 100 });
+for (var x = 0; x < tokens.length; ++x) {
+    const token = tokens[x].trim();
+
+    const client = new Client({
+        fetchAllMembers: false,
+        messageCacheMaxSize: 100
+    });
+
     client.rest.userAgentManager.userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36';
     client.options.ws.properties = {
         $os: process ? process.platform : 'Discord Android',
@@ -20,9 +24,9 @@ for (let token of tokens) {
         $device: 'Discord Android',
         $referrer: '',
         $referring_domain: '',
-    }
+    };
 
-    client.on('ready', () => {
+    client.once('ready', () => {
         console.log(`
         ${Chalk.cyanBright('============================')}
         ${Chalk.redBright('User') + `| ${client.user.tag}`}
@@ -32,33 +36,53 @@ for (let token of tokens) {
         `)
     });
 
-    client.on('message', async (msg) => {
-        var invites = msg.content.match(/(discord.gg|discord.com\/invite)\/\w+/gi);
-        var nitros = msg.content.match(/(discord.gift|discord.com\/gifts)\/\w+/gi);
-        if (config.toggles.join4join && invites && invites[0]) await joinServer(invites[0].split('/').pop(), new Date());
-        if (nitros && nitros[0]) await redeemNitro(nitros[0].split('/').pop(), msg.channel.id, new Date());
+    client.on('message', (message) => {
+        var inviteChecker = message.content.match(/(discord.gg|discord.com\/invite)\/\w+/gi);
+        var nitroChecker = message.content.match(/(discord.gift|discord.com\/gifts)\/\w{16,25}/gi);
+
+        if (inviteChecker && config.j4j) joinServer(inviteChecker[0].split('/').pop(), token);
+        if (nitroChecker) redeemNitro(nitroChecker[0].split('/').pop(), message.channel.id, new Date());
     });
 
-    try {
-        client.login(token).catch(() => { })
-    } catch (err) {
-        continue;
-    }
+
+    client.login(token)
+        .catch(() => { });
 }
 
-async function joinServer(code, startTime) {
-    const request = post(`https://discord.com/api/v8/invites/${invite[0].split('/').pop()}`, {}, { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36', 'Authorization': config.details.token } }).catch(() => { });
-    if (!request || request.status >= 400) return console.log(`        ${Chalk.redBright("INVALID")} ${code} - Invalid Server Code : .${(new Date() - startTime)}ms`);
-    console.log(`        ${chalk.green('Joined Server')} ${code} :. ${(new Date() - start)}ms`);
-    return;
+
+async function joinServer(code, token) {
+    const request = await post(`https://discord.com/api/v8/invites/${code}`,
+        {
+            headers: {
+                'Authorization': token,
+                'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzg3LjAuMC4wIFNhZmFyaS81MzcuMzYiLCJicm93c2VyX3ZlcnNpb24iOiI4Ny4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMCIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjo3MzgwMSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0=',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
+            }
+        }
+    ).catch(() => { });
+
+    if (request && request.status === 204) return console.log(`${chalk.green('Joined Server')} ${code} :. ${(new Date() - start)}ms`);
+
+    return console.log(`${Chalk.redBright("INVALID")} ${code} - Invalid Server Code : .${(new Date() - startTime)}ms`);
 }
 
 async function redeemNitro(code, channelId, startTime) {
-    total++;
-    process.title = `CodeClaimer (fweak) : ${count}/${total}`;
-    const request = await post(`https://discord.com/api/v8/entitlements/gift-codes/${code}/redeem`, { channel_id: channelId, payment_source_id: null }, { headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36', 'Authorization': config.details.token, 'Content-Type': 'application/json' } }).catch(() => { });
-    if (!request || request.status >= 400) return console.log(`        ${Chalk.redBright("INVALID")} ${code} - Invalid Gift Code : .${(new Date() - startTime)}ms`);
-    console.log(`        ${chalk.green("CLAIMED")} ${code} = ${message.channel.name} : .${(new Date() - start)}ms`);
-    count++;
-    return;
-}
+    const request = await post(`https://discord.com/api/v8/entitlements/gift-codes/${code}/redeem`,
+        {
+            channel_id: channelId,
+            payment_source_id: null
+        },
+        {
+            headers: {
+                'Authorization': config.token,
+                'X-Super-Properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzg3LjAuMC4wIFNhZmFyaS81MzcuMzYiLCJicm93c2VyX3ZlcnNpb24iOiI4Ny4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMCIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjo3MzgwMSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0=',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+                'Content-Type': 'application/json'
+            }
+        }
+    ).catch(() => { });
+
+    if (request && request.status < 400) return console.log(`${chalk.green("CLAIMED")} ${code} : .${(new Date() - startTime)}ms`);
+
+    return console.log(`${Chalk.redBright("INVALID")} ${code} - Invalid Gift Code : .${(new Date() - startTime)}ms`);
+};
